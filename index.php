0f6a804736568699c3f4dc539f1b6dce8ec0b9e9
geass991155 <33750225+geass991155@users.noreply.github.com>
2022-10-03 15:19:23 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="zh-Hant-TW">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=2.0, user-scalable=yes">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>LineLogin</title>
+</head>
+<body>
+  嗨這是首頁
+</body>
+</html>
+
