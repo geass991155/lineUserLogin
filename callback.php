42eac65d4e97901854424e0b655c6d0493184c9f
@Kelly <geass991155@gmail.com>
2022-10-04 16:44:41 +0800

    刪掉foreach

--- callback.php
+++ callback.php
@@ -1,5 +1,5 @@
 <?php
-session_start();//啟用Session功能
+session_start(); //啟用Session功能
 // 用code取得access_token，access_token取得用戶資料
 $code = $_GET["code"];
 $state = $_GET["state"];
@@ -7,29 +7,26 @@ $state = $_GET["state"];
 include_once('include/LineProfiles.php'); //取得用戶端 Profile
 include_once('include/config.php'); //設定值
 
-$result = getAccessToken($code,$config); //取得使用者資料
+$result = getAccessToken($code, $config); //取得使用者資料
 
 setcookie("access_token", $result["0"], time() + 3600 * 24 * 20); //把他記憶20天
 $user = getLineProfile_access_token($result["0"]); //取得使用者資料
-$userId="";
-foreach($user as $key => $value) {
-	if ($key == "userId") {
-		$userId = $value;
-	}
-}
+$userId = $user["userId"];
+
 $send = sendMessage($userId, $config);
 
 $_SESSION['displayName'] = $user["displayName"];
 $_SESSION['userId'] = $user["userId"];
 $_SESSION['email'] = $result["1"]->email;
+$_SESSION['access_token'] = $result["0"];
 
 if (!$code) {
-	echo "<script> window.alert('code錯誤');";
+	echo "<script> window.alert('code錯誤'); ";
 	echo "location.href='index.php';</script>";
-} 
+}
 
-if($_SESSION['userId']) {
-	echo "<script> location.href='product.php';</script>";
+if ($_SESSION['userId']) {
+	echo "<script> location.href = 'product.php';</script>";
 }
 ?>
 <!DOCTYPE html>
@@ -46,30 +43,30 @@ if($_SESSION['userId']) {
 	<h1><a href="./index.php">回首頁</a></h1>
 	使用者資料
 	<?php
-	if ($user) {
-		
-		echo "<pre>";
-		print_r($user);
-		echo "</pre>";
-		print_r($user["userId"]);
-		
-	}
-	else {
-		echo "沒有";
-	}
+if ($user) {
 
-	if ($result["1"]) {
-		echo "<pre>";
-		print_r($result["1"]);
-		echo "</pre>";
-		
-		echo "<pre>";
-		print_r($result["1"]->email);
-		echo "</pre>";
+	echo "<pre>";
+	print_r($user);
+	echo "</pre>";
+	print_r($user["userId"]);
 
-		echo $_SESSION['displayName'];
-	}
-	?>
+}
+else {
+	echo "沒有";
+}
+
+if ($result["1"]) {
+	echo "<pre>";
+	print_r($result["1"]);
+	echo "</pre>";
+
+	echo "<pre>";
+	print_r($result["1"]->email);
+	echo "</pre>";
+
+	echo $_SESSION['displayName'];
+}
+?>
 
 </body>
 
